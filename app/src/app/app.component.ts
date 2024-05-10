659d64a037c53568eefbb1bdfdf8b247483621cf
arodrica43 <lexws33@gmail.com>
2024-05-10 00:15:54 +0000

    adding some style

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,22 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'portfolio';
-
-  constructor(private modalService: NgbModal) {
-  }
-
-  public open(modal: any): void {
-    this.modalService.open(modal);
-  }
+  title = 'app';
 }
